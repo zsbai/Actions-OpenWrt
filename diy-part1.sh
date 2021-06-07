b31e206f617ae521d885defa57c844aa0ceaa2e8
zsbai <62942942+zsbai@users.noreply.github.com>
2021-06-07 20:51:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,5 @@
 git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
 git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git luci-app-v2ray
 git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+git clone https://github.com/vernesong/OpenClash.git
+mv /OpenClash/luci-app-openclash package/
