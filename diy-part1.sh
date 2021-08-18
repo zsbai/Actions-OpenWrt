8affaf1c43b2bfc21af35ba1088a60557af7a354
zsbai <62942942+zsbai@users.noreply.github.com>
2021-08-18 16:05:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,12 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+ 
+sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 
-
-git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
-git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
+# git clone https://github.com/kuoruan/openwrt-v2ray.git package/v2ray-core
+# git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/luci-app-v2ray
 git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
-git clone https://github.com/vernesong/OpenClash.git
-mv ./OpenClash/luci-app-openclash ./package/
-rm -rf OpenClash
+# git clone https://github.com/vernesong/OpenClash.git
+# mv ./OpenClash/luci-app-openclash ./package/
+# rm -rf OpenClash
